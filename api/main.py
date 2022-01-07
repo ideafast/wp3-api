2bbb7d6fe1d1716ab68108f3b4c20909fc6f4e0e
David <davidverweij@gmail.com>
2022-01-07 17:25:28 +0000

    prep for repeated faq/doc cloning

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi_utils.tasks import repeat_every
 
 from api.docs import router as docs
 from api.patients import router as patients
@@ -9,3 +10,10 @@ api = FastAPI(docs_url="/swagger", redoc_url="/redoc")
 api.include_router(patients, prefix="/patients")
 api.include_router(docs, prefix="/docs")
 api.include_router(pipeline, prefix="/status")
+
+
+@api.on_event("startup")
+@repeat_every(seconds=86400)
+def get_latest_docs() -> None:
+    """Clone the documentation repo on a daily basis to serve over API"""
+    print("cloning FAQ and DOC repository")
