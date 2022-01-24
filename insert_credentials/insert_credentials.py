a84b8d814eb2a0bb03d187e90e0d15411469bb48
Colin Bone Dodds <colin8ll@yahoo.co.uk>
2022-01-24 15:30:42 +0000

    functionality to add participant credentials to the mongo db

---
+++ insert_credentials/insert_credentials.py
@@ -0,0 +1,37 @@
+import csv
+import os
+
+import pymongo
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# myclient = pymongo.MongoClient("mongodb://colin:colin_pass@localhost:27017/")
+myclient = pymongo.MongoClient(
+    host=os.getenv("_MONGO_INITDB_HOST"),
+    username=os.getenv("_MONGO_INITDB_ROOT_USERNAME"),
+    password=os.getenv("_MONGO_INITDB_ROOT_PASSWORD"),
+)
+mydb = myclient[os.getenv("_MONGO_INITDB_DATABASE")]
+mycol = mydb[os.getenv("_MONGO_INITDB_COLLECTION")]
+
+data = []
+
+# open CSV file
+with open("./credentials/credentials.csv") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    next(csv_reader)  # skip the first row (headers)
+    for row in csv_reader:
+        new_row = {
+            "patient_id": row[0],
+            "dreem_email": row[1],
+            "dreem_password": row[2],
+            "wildkeys_email": row[3],
+            "wildkeys_password": row[4],
+            "tfa_email": row[5],
+            "tfa_password": row[6],
+        }
+        data.append(new_row)
+
+# insert the data
+mycol.insert_many(data)
